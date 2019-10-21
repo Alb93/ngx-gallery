741ec3156fa1396fbcc4ccfb4afb330b020ba44e
alberto di gioacchino <alberto.digioacchino.93@gmail.com>
2019-10-21 17:18:52 +0200

    Remove dist folder. Add files on package.json.

--- dist/ngx-gallery.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './index';
-export { EnhanceBackgroundDirective as ɵa } from './enhance-background.directive';
-export { SrcFallbackDirective as ɵb } from './src-fallback.directive';
